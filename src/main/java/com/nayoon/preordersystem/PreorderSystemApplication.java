7657360038a1c823c28a012b56a6896f651a564d
Nayoon <nayoonk928@gmail.com>
2024-01-26 01:56:03 +0900

    Initial commit

---
+++ src/main/java/com/nayoon/preordersystem/PreorderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nayoon.preordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreorderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreorderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nayoon/preordersystem/PreorderSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nayoon.preordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PreorderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
