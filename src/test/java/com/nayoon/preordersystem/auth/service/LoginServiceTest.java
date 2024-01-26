f5121e399c1d7fb0f368527e36eecd48494bb9b9
Nayoon <nayoonk928@gmail.com>
2024-01-26 12:38:25 +0900

    chore #3 - LoginService 테스트 코드 메서드 수정

--- src/test/java/com/nayoon/preordersystem/auth/service/LoginServiceTest.java
+++ src/test/java/com/nayoon/preordersystem/auth/service/LoginServiceTest.java
@@ -74,8 +74,7 @@ class LoginServiceTest {
 
       when(userRepository.findByEmail(request.email())).thenReturn(Optional.of(user));
       when(EncryptionUtils.matchPassword(request.password(), user.getPassword())).thenReturn(true);
-      when(jwtTokenProvider.generateToken(user.getEmail(), user.getName(),
-          user.getUserRole())).thenReturn(tokenDto);
+      when(jwtTokenProvider.generateToken(user.getEmail(), user.getUserRole())).thenReturn(tokenDto);
 
       //when
       TokenDto result = loginService.login(request);
