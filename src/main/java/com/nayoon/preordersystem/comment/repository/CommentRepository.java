6816c33f3dec4ff261531a35256e089edf3645e8
Nayoon <nayoonk928@gmail.com>
2024-02-01 03:24:38 +0900

    refactor #18 - root 프로젝트 src 폴더 제거

--- src/main/java/com/nayoon/preordersystem/comment/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nayoon.preordersystem.comment.repository;
-
-import com.nayoon.preordersystem.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
--- src/main/java/com/nayoon/preordersystem/post/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nayoon.preordersystem.post.repository;
-
-import com.nayoon.preordersystem.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
