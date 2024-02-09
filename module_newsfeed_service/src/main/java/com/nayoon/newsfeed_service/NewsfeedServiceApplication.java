238a3387c506c61c7dd748205e698f0a54ffedf7
Nayoon <nayoonk928@gmail.com>
2024-02-10 00:55:22 +0900

    chore #26 - docker 빌드

--- module_newsfeed_service/src/main/java/com/nayoon/newsfeed_service/NewsfeedServiceApplication.java
+++ module_newsfeed_service/src/main/java/com/nayoon/newsfeed_service/NewsfeedServiceApplication.java
@@ -3,9 +3,11 @@ package com.nayoon.newsfeed_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class NewsfeedServiceApplication {
 
   public static void main(String[] args) {
