3570336719444961a1ffc877fecb52198c867bd5
Nayoon <nayoonk928@gmail.com>
2024-02-12 03:15:29 +0900

    test #26 - 상품 조회 기능 단위 테스트

--- module_ecommerce_service/src/test/java/com/nayoon/ecommerce_service/product/service/ProductServiceTest.java
+++ module_ecommerce_service/src/test/java/com/nayoon/ecommerce_service/product/service/ProductServiceTest.java
@@ -1,18 +1,26 @@
 package com.nayoon.ecommerce_service.product.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.nayoon.ecommerce_service.common.exception.CustomException;
+import com.nayoon.ecommerce_service.common.exception.ErrorCode;
 import com.nayoon.ecommerce_service.product.dto.request.ProductCreateRequest;
 import com.nayoon.ecommerce_service.product.dto.request.ProductUpdateRequest;
+import com.nayoon.ecommerce_service.product.dto.response.ProductResponse;
+import com.nayoon.ecommerce_service.product.dto.response.ProductStockResponse;
 import com.nayoon.ecommerce_service.product.entity.Product;
 import com.nayoon.ecommerce_service.product.entity.ProductStock;
 import com.nayoon.ecommerce_service.product.repository.ProductRepository;
 import com.nayoon.ecommerce_service.product.repository.ProductStockRepository;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -21,6 +29,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -46,7 +57,8 @@ class ProductServiceTest {
       Product product = mockProduct();
 
       when(productRepository.save(any(Product.class))).thenAnswer(invocation -> {
-        return Product.builder().name(request.name()).content(request.content()).price(request.price()).build();
+        return Product.builder().name(request.name()).content(request.content())
+            .price(request.price()).build();
       });
       when(productStockRepository.save(any(ProductStock.class))).thenAnswer(invocation -> {
         return ProductStock.builder().productId(product.getId()).stock(request.stock()).build();
@@ -90,6 +102,116 @@ class ProductServiceTest {
       verify(productStockRepository, times(1)).save(any(ProductStock.class));
     }
 
+  }
+
+  @Nested
+  @DisplayName("전체 상품 조회")
+  class getAllProducts {
+
+    @Test
+    @DisplayName("성공")
+    void success() {
+      //given
+      Pageable pageable = Pageable.ofSize(10).withPage(0);
+
+      // Mock data for products
+      List<Product> products = Arrays.asList(
+          mockProduct(),
+          mockProduct2()
+      );
+      when(productRepository.filterAllProducts(eq(pageable))).thenReturn(new PageImpl<>(products));
+
+      //when
+      Page<Product> result = productService.getAllProducts(pageable);
+
+      //then
+      assertEquals(products, result.getContent());
+    }
+
+  }
+
+  @Nested
+  @DisplayName("상품 재고 조회")
+  class getProductStockById {
+
+    @Test
+    @DisplayName("성공")
+    void success() {
+      //given
+      Long productId = 1L;
+      ProductStock productStock = new ProductStock(productId, 100);
+
+      when(productStockRepository.findById(productId)).thenReturn(Optional.of(productStock));
+
+      //when
+      ProductStockResponse response = productService.getProductStockById(productId);
+
+      //then
+      assertEquals(productId, response.productId());
+      assertEquals(productStock.getStock(), response.stock());
+    }
+
+    @Test
+    @DisplayName("실패: 재고 정보 찾을 수 없음")
+    void productStockNotFound() {
+      //given
+      Long productId = 1L;
+
+      //when
+      CustomException exception = assertThrows(CustomException.class, ()
+          -> productService.getProductStockById(productId));
+
+      //then
+      assertEquals(ErrorCode.PRODUCT_STOCK_NOT_FOUND, exception.getErrorCode());
+    }
+
+  }
+
+  @Nested
+  @DisplayName("상품 상세정보 조회")
+  class getProductInfoById {
+
+    @Test
+    @DisplayName("성공")
+    void success() {
+      //given
+      Long productId = 1L;
+      Product product = mockProduct();
+
+      when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+      //when
+      ProductResponse response = productService.getProductInfoById(productId);
+
+      //then
+      assertEquals(product.getName(), response.name());
+      assertEquals(product.getContent(), response.content());
+      assertEquals(product.getPrice(), response.price());
+    }
+
+    @Test
+    @DisplayName("실패: 상품 정보 찾을 수 없음")
+    void productNotFound() {
+      //given
+      Long productId = 1L;
+
+      //when
+      CustomException exception = assertThrows(CustomException.class, ()
+          -> productService.getProductInfoById(productId));
+
+      //then
+      assertEquals(ErrorCode.PRODUCT_NOT_FOUND, exception.getErrorCode());
+    }
+
+  }
+
+  private Product mockProduct2() {
+    return Product.builder()
+        .userId(1L)
+        .name("테스트 상품2")
+        .content("테스트 상품2 내용")
+        .price(20000L)
+        .build();
   }
 
   private Product mockProduct() {
