c925a38cc0f664739089437f334fa30fcf82f474
Nayoon <nayoonk928@gmail.com>
2024-02-13 12:43:44 +0900

    chore - repository 분리

--- module_ecommerce_service/src/main/java/com/nayoon/ecommerce_service/product/repository/ProductStockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nayoon.ecommerce_service.product.repository;
-
-import com.nayoon.ecommerce_service.product.entity.ProductStock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductStockRepository extends JpaRepository<ProductStock, Long> {
-
-}
